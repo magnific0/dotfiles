e6b9eec78cb7f7b4503988f828fb2cd86d5c0ce1
Jacco Geul <jacco@geul.net>
2015-12-23 13:51:18 +0100

    Change behaviour for zero and one keyboard. Filter out virtuals.

--- scripts/.scripts/toggle_kb.sh
+++ scripts/.scripts/toggle_kb.sh
@@ -5,17 +5,26 @@ KB_LEN=${#KB_LAY[@]}
 
 [[ -f ${HOME}/.dmenurc ]] && . ${HOME}/.dmenurc || DMENU='dmenu -i'
 
-KB_LST=$(xinput list | grep -n keyboard | grep -vi button | grep -vi hotkeys | \
-         grep -vi bus | grep -vi webcam | awk -F: '{ print $1 }')
+KB_LST=$(xinput list | grep -n keyboard | grep -vi button | grep -vi hotkeys \
+        | grep -vi bus | grep -vi webcam | grep -vi virtual \
+        | awk -F: '{ print $1 }')
 KB_END=$(echo "${KB_LST}" | tail -1 | awk -F: '{ print $1 }')
 KB_NAM=$(echo "${KB_LST}" | xargs -I{} sh -c "xinput list --name-only | sed -n -e {}p")
 KB_IDS=$(echo "${KB_LST}" | xargs -I{} sh -c "xinput list --id-only | sed -n -e {}p")
 
-# Get choice from DMENU, exit if DMENU is quit improperly (e.g. ESC)
-res=$(echo "${KB_NAM}" | ${DMENU} -p "Select keyboard") || exit $?
+KB_NUM=$(echo "${KB_NAM}" | wc -l)
+if [ "${KB_NUM}" -eq "0" ]; then
+  echo "No keyboards found"
+  exit 1
+elif [ "${KB_NUM}" -eq "1" ]; then
+  echo "Only one keyboard found, not really a choice, eh?"
+  res="${KB_NAM}"
+else
+  # Get choice from DMENU, exit if DMENU is quit improperly (e.g. ESC)
+  res=$(echo "${KB_NAM}" | ${DMENU} -p "Select keyboard") || exit $?
+fi
 
 KB_SID=$(echo "${KB_NAM}" | grep -n "${res}" | head -1 | awk -F: '{ print $1 }')
-
 KB_EID=$(echo "${KB_IDS}" | awk "NR==${KB_SID}") 
 
 if [ -n "$KB_EID" ]; then
@@ -29,5 +38,5 @@ if [ -n "$KB_EID" ]; then
     done
     setxkbmap -device ${KB_EID} -option compose:caps
 else
-    echo "ErgoDox not found!"
+    echo "Keyboard not found!"
 fi
